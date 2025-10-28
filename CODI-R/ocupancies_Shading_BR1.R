aa679d802ca07203d1502fc404c4ff3d2762c86f
vankampen92 <dalonso@ceab.csic.es>
2025-10-28 17:19:06 +0100

    little change in occupancies_Shading_BR1.R

--- CODI-R/ocupancies_Shading_BR1.R
+++ CODI-R/ocupancies_Shading_BR1.R
@@ -1,12 +1,13 @@
 
 #END CALCUL OCUPANCIES Plebejus argus ##################################
-#####################
 #
 # Funcio R: Ocupancia teorica en funcio del temps quan la condicio initial es una p_0 generica:
-p <- function(c, e, t, p_0) {
-  value <- (c / (e + c)) * (1 - exp(-(e + c) * t)) + p_0 * exp(-(e + c) * t))
+p_occupancy_ce <- function(c, e, t, p_0) {
+  value <- (c / (e + c)) * (1 - exp(-(e + c) * t)) + p_0 * exp(-(e + c) * t)
   return(value)
 }
+
+
 
 
 
